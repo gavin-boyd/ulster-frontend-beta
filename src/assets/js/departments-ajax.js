214487e4228a0d2b8baf91044bde483227822126
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2018-05-04 16:05:08 +0100

    latest stable build

---
+++ src/assets/js/departments-ajax.js
@@ -0,0 +1,86 @@
+jQuery(document).ready(function() {
+  function updatePageAndHistory(data, ajaxUrl, pushState) {
+      if (pushState) {
+          history.pushState({}, '', ajaxUrl);
+      }
+      var results = jQuery(data).find('#content-view');
+      jQuery('#content-view').replaceWith(results);
+  }
+  var ajax;
+  function ajaxRequest(searchURL, pushState) {
+      jQuery('.departments a').addClass('init');
+      if (!('history' in window) || !('pushState' in history)) {
+        location.assign(searchURL);
+      } else {
+        if (ajax && ajax.readyState != 4) {
+          ajax.abort();
+        }
+        ajax = jQuery.get(searchURL, function(data) {
+            var payload = jQuery(jQuery.parseHTML(data));
+            updatePageAndHistory(payload, this.url, pushState);
+            var elem = new Foundation.Equalizer(jQuery('.staff-cards'));
+            //clickEvents();
+            //debug
+            //console.log('success');
+        }).done(function() {
+            //debug
+            //console.log('data loaded');
+            jQuery('.departments a').removeClass('init');
+        }).fail(function() {
+            //debug
+            //console.log('there was a problem');
+            //location.reload();
+        }).always(function() {
+            //debug
+            //console.log('finished');
+        });
+        jQuery('#content-view').prepend('<div class="preloader-container"><div class="preloader"><span class="hide">Loading search results</span> <img src="//cdn.ulster.ac.uk/home/ulster-frontend/beta/assets/img/cool-preloader.gif" width="100" alt="Loading results"/></div></div>');
+      }
+  }
+  window.onpopstate = function(event) {
+    ajaxRequest(document.location, false);
+  };
+  jQuery('#departments').submit(function(e) {
+      e.preventDefault();
+      ajaxRequest(jQuery(this).attr('action'), true);
+  });
+  function clickEvents() {
+      jQuery('.departments>ul>ul>ul a').click(function(e) {
+          e.preventDefault();
+          jQuery('li.active').removeClass('active');
+          jQuery('ul.active-ul').removeClass('active-ul');
+          ajaxRequest(jQuery(this).attr('href'), true);
+          jQuery('.active').removeClass('active');
+          jQuery(this).parent('li').addClass('active');
+          jQuery("html, body").animate({ scrollTop: 0 }, "slow");
+          jQuery(this).parent('li').parent('ul').parent('ul').children('li').addClass('active');
+          jQuery(this).parents('ul').addClass('active-ul');
+          jQuery('.departments>ul>ul').each(function() {
+            if (jQuery(this).hasClass('active-ul')) {
+            } else {
+              jQuery(this).addClass('hide-departments');
+            }
+          });
+      });
+  }
+  clickEvents();
+  jQuery('.departments>ul>ul').each(function() {
+    jQuery(this).addClass('hide-departments');
+  });
+  jQuery('.departments>ul>ul>li>a').each(function() {
+    jQuery(this).click(function(e) {
+      e.preventDefault();
+      jQuery('.departments>ul>ul').each(function() {
+        if (jQuery(this).hasClass('active-ul')) {
+        } else {
+          jQuery(this).addClass('hide-departments');
+        }
+      });
+      if (jQuery(this).parent('li').parent('ul').hasClass('hide-departments')) {
+        jQuery(this).parent('li').parent('ul').removeClass('hide-departments');
+      } else {
+        jQuery(this).parent('li').parent('ul').addClass('hide-departments');
+      }
+    });
+  });
+});
