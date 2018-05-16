22e43f093155eda088ea9e6c9956d155befab627
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2018-05-16 11:57:08 +0100

    latest stable build - video component

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -26,3 +26,68 @@ jQuery('[data-fancybox]').fancybox({
 		preload : false
 	}
 });
+
+/*
+  Youtube - https://www.youtube.com/iframe_api
+*/
+
+function script(url) {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = false;
+    s.src = url;
+    var x = document.getElementsByTagName('head')[0];
+    x.appendChild(s);
+}
+script('//www.youtube.com/iframe_api');
+
+//sticky video
+jQuery(document).ready(function() {
+  if (jQuery('#featured-media').length > 0) {
+    var thewindow = jQuery(window); // 1. Window Object.
+    var featuredMedia = jQuery("#featured-media"); // 1. The Video Container.
+    var featuredVideo = jQuery("#featured-video"); // 2. The Youtube Video.
+
+    var player; // 3. Youtube player object.
+    var top = featuredMedia.offset().top; // 4. The video position from the top of the document;
+    var offset = Math.floor(top + (featuredMedia.outerHeight() / 2)); //5. offset.
+
+    window.onYouTubeIframeAPIReady = function() {
+      player = new YT.Player("featured-video", {
+         events: {
+           "onStateChange": onPlayerStateChange
+         }
+      });
+    };
+
+    /**
+     * Run when the Youtube video state (play, pause, etc.) is changed.
+     *
+     * @param {Object} event The Youtube Object Event.
+     * @return {Void}
+     */
+    function onPlayerStateChange( event ) {
+       var isPlay  = 1 === event.data;
+       var isPause = 2 === event.data;
+       var isEnd   = 0 === event.data;
+       if (isPlay) {
+          featuredVideo.removeClass("is-paused");
+          featuredVideo.toggleClass("is-playing");
+       }
+       if (isPause) {
+          featuredVideo.removeClass("is-playing");
+          featuredVideo.toggleClass("is-paused");
+       }
+       if (isEnd) {
+          featuredVideo.removeClass("is-playing", "is-paused");
+       }
+    }
+
+    thewindow.on("resize", function() {
+       top = featuredMedia.offset().top;
+       offset = Math.floor(top + ( featuredMedia.outerHeight() / 2 ) );
+    }).on("scroll", function() {
+       featuredVideo.toggleClass("is-sticky", thewindow.scrollTop() > offset);
+    });
+  }
+});
--- src/matrix/Agent (Id: #280197).js
+++ src/matrix/Agent (Id: #280197).js
@@ -1,56 +1,110 @@
 var res = eval('(' + _REST.response.body + ')');
 var country_code = "%globals_asset_metadata_Ulster.Capture.Country^escapequotes%";
 var output = '';
+var showGeneric = [];
 if (res) {
-    for (i = 0; i < res.length; i++) {
-        var agent_country_code = res[i].country_code;
-        var agent_country_code_array = agent_country_code.split('; ');
-        for (x = 0; x < agent_country_code_array.length; x++) {
-          if (country_code == agent_country_code_array[x]) {
-            output += '<div class="bg-purple">';
-              output += '<div class="grid-container white-text">';
+  for (i = 0; i < res.length; i++) {
+    var agent_country_code = res[i].country_code;
+    var agent_country_code_array = agent_country_code.split('; ');
+    for (x = 0; x < agent_country_code_array.length; x++) {
+      var agent_code = agent_country_code_array[x];
+      agent_code = String(agent_code);
+      if (country_code == agent_code) {
+        showGeneric.push('true');
+        output += '<div class="">';
+          output += '<div class="grid-container">';
+            output += '<div class="grid-x grid-padding-x grid-padding-y">';
+              output += '<div class="cell medium-12">';
+                output += '<br><br>';
+                output += '<h2 class="h1">Hi %globals_asset_metadata_Ulster.Capture.First.Name^escapequotes%, welcome to Ulster University</h2>';
+                output += '<br>';
                 output += '<div class="grid-x grid-padding-x grid-padding-y">';
-                  output += '<div class="cell medium-12">';
-                    output += '<br><br>';
-                    output += '<h2 class="h1">Hi %globals_asset_metadata_Ulster.Capture.Name%, welcome to Ulster University</h2>';
-                    output += '<br>';
-                    output += '<div class="grid-x grid-padding-x grid-padding-y">';
-                      output += '<div class="cell large-2">';
-                        output += '<img src="' + res[i].image + '" alt="' + res[i].full_name + '" class="circle">';
-                      output += '</div>';
-                    output += '<div class="cell large-6">';
-                      output += '<blockquote>';
-                        output += '<cite>';
-                          output += '<span class="h3">';
-                            output += res[i].prospectus_intro;
-                          output += '</span>';
-                        output += '</cite>';
-                        output += res[i].prospectus_content;
-                      output += '</blockquote>';
-                    output += '</div>';
-                    output += '<div class="cell large-4">';
-                      if (res[i].phone !== '') {
-                        var mobile = res[i].phone;
-                        mobile = mobile.replace(/\s/g, '');
-                        mobile = mobile.replace('+', '');
-                        output += '<a href="https://api.whatsapp.com/send?phone=' + mobile + '" class="button large expanded"><i class="fa fa-whatsapp" aria-hidden="true"></i> WhatsApp</a>';
-                        output += '<a href="#" class="button large expanded"><i class="fa fa-weixin" aria-hidden="true"></i> WeChat</a>';
-                      }
-                      if (res[i].email !== '') {
-                        output += '<a href="mailto:' + res[i].email + '" class="button large expanded"><i class="fa fa-envelope-o" aria-hidden="true"></i> Email</a>';
-                      }
-                      if (res[i].facebook !== '') {
-                        output += '<a href="https://m.me/' + res[i].facebook + '" class="button large expanded" target="_blank><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a>';
-                      }
-                    output += '</div>';
+                  output += '<div class="cell large-2">';
+                    output += '<img src="' + res[i].image + '" alt="' + res[i].full_name + '" class="circle">';
                   output += '</div>';
+                output += '<div class="cell large-6">';
+                  output += '<blockquote>';
+                    output += '<cite>';
+                      output += '<span class="h3">';
+                        output += res[i].prospectus_intro;
+                      output += '</span>';
+                    output += '</cite>';
+                    output += '<br>';
+                    output += res[i].prospectus_content;
+                  output += '</blockquote>';
+                output += '</div>';
+                output += '<div class="cell large-4">';
+                  if (res[i].phone !== '') {
+                    var mobile = res[i].phone;
+                    mobile = mobile.replace(/\s/g, '');
+                    mobile = mobile.replace('+', '');
+                    output += '<a href="https://api.whatsapp.com/send?phone=' + mobile + '" class="button large expanded"><i class="fa fa-whatsapp" aria-hidden="true"></i> WhatsApp</a>';
+                    //output += '<a href="#" class="button large expanded"><i class="fa fa-weixin" aria-hidden="true"></i> WeChat</a>';
+                  }
+                  if (res[i].email !== '') {
+                    output += '<a href="mailto:' + res[i].email + '" class="button large expanded"><i class="fa fa-envelope-o" aria-hidden="true"></i> Email</a>';
+                  }
+                  if (res[i].facebook !== '') {
+                    output += '<a href="https://m.me/' + res[i].facebook + '" class="button large expanded" target="_blank><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a>';
+                  }
                 output += '</div>';
               output += '</div>';
             output += '</div>';
-            output += '<br><br>';
-            output += '</div>';
-            print(output);
-          }
-        }
+          output += '</div>';
+        output += '</div>';
+        output += '<br><br>';
+        output += '</div>';
+        print(output);
+      }
     }
+  }
+  if (showGeneric.indexOf('true') == -1) {
+    var outputTwo = '';
+    outputTwo += '<div class="">';
+      outputTwo += '<div class="grid-container">';
+        outputTwo += '<div class="grid-x grid-padding-x grid-padding-y">';
+          outputTwo += '<div class="cell medium-12">';
+            outputTwo += '<br><br>';
+            outputTwo += '<h2 class="h1">Hi %globals_asset_metadata_Ulster.Capture.First.Name^escapequotes%, welcome to Ulster University</h2>';
+            outputTwo += '<br>';
+            outputTwo += '<div class="grid-x grid-padding-x grid-padding-y">';
+              outputTwo += '<div class="cell large-2">';
+                outputTwo += '<img src="%globals_asset_assetid:284270^as_asset:asset_thumbnail_url%" alt="" class="circle">';
+              outputTwo += '</div>';
+            outputTwo += '<div class="cell large-6">';
+              outputTwo += '<blockquote>';
+                outputTwo += '<cite>';
+                  outputTwo += '<span class="h3">';
+                    outputTwo += '%globals_asset_assetid:284270^as_asset:asset_metadata_Ulster.International.Agent.Digital.Prospectus.Intro^escapequotes%';
+                  outputTwo += '</span>';
+                outputTwo += '</cite>';
+                outputTwo += '<br>';
+                outputTwo += '%globals_asset_assetid:284270^as_asset:asset_metadata_Ulster.International.Agent.Digital.Prospectus.Content^escapequotes%';
+              outputTwo += '</blockquote>';
+            outputTwo += '</div>';
+            outputTwo += '<div class="cell large-4">';
+              var phone = '%globals_asset_assetid:284270^as_asset:asset_metadata_Ulster.International.Agent.Phone%';
+              if (phone !== '') {
+                var mobile = res[i].phone;
+                mobile = mobile.replace(/\s/g, '');
+                mobile = mobile.replace('+', '');
+                outputTwo += '<a href="https://api.whatsapp.com/send?phone=' + phone + '" class="button large expanded"><i class="fa fa-whatsapp" aria-hidden="true"></i> WhatsApp</a>';
+              }
+              var email = '%globals_asset_assetid:284270^as_asset:asset_metadata_Ulster.International.Agent.Email%';
+              if (email !== '') {
+                outputTwo += '<a href="mailto:' + email + '" class="button large expanded"><i class="fa fa-envelope-o" aria-hidden="true"></i> Email</a>';
+              }
+              var facebook = '%globals_asset_assetid:284270^as_asset:asset_metadata_Ulster.International.Agent.Facebook%';
+              if (facebook !== '') {
+                outputTwo += '<a href="https://m.me/' + facebook + '" class="button large expanded" target="_blank><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a>';
+              }
+            outputTwo += '</div>';
+          outputTwo += '</div>';
+        outputTwo += '</div>';
+      outputTwo += '</div>';
+    outputTwo += '</div>';
+    outputTwo += '<br><br>';
+    outputTwo += '</div>';
+    print(outputTwo);
+  }
 }
