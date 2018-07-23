eb178917b8f0cb2e5aba8efec75f965c02b7ef70
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2018-07-23 14:50:51 +0100

    latest stable build

--- src/matrix/Graduations (Id: #285287).js
+++ src/matrix/Graduations (Id: #285287).js
@@ -1,6 +1,8 @@
 var res = eval('(' + _REST.response.body + ')');
 var campus = "%globals_asset_name^replace: campus:%";
 var season = "%globals_asset_assetid:242835^as_asset:asset_metadata_Graduation.Season%";
+var summer_end = "%globals_asset_assetid:242835^as_asset:asset_metadata_Graduation.Summer.Date.To%";
+var winter_end = "%globals_asset_assetid:242835^as_asset:asset_metadata_Graduation.Winter.Date.To%";
 season = season.toLowerCase();
 var output = '';
 if (res) {
@@ -9,10 +11,23 @@ if (res) {
       for (i = 0; i < res.length; i++) {
         var api_season = res[i].season;
         if (api_season == season) {
+          var end_date = '';
+          var today = new Date();
+          today.setHours(0,0,0,0);
+          if (api_season == 'Summer') {
+            end_date = summer_end;
+          }
+          if (api_season == 'Winter') {
+            end_date = winter_end;
+          }
+          if (end_date !== '') {
+            end_date = new Date(end_date);
+            end_date.setHours(0,0,0,0);
+          }
           var api_campus = res[i].campus;
           api_campus = api_campus.split('; ');
           api_campus.forEach(function(value) {
-            if (value == campus) {
+            if (value == campus && end_date >= today) {
               output += '<div class="cell large-6">';
                 output += '<div class="pad-60">';
                   output += '<h2 class="separator-left">' + res[i].asset_name + '</h2>';
--- src/matrix/Ulster.Your.Country/Ulster.Your.Country.Global.Agents.js
+++ src/matrix/Ulster.Your.Country/Ulster.Your.Country.Global.Agents.js
@@ -5,30 +5,49 @@ if (res) {
     var output = '<section id="agentcontacts" data-magellan-target="agentcontacts">';
     var title = '';
     var total = 0;
-    showNavitem = false;
     for (i = 0; i < res.length; i++) {
-      var agent_country_code = res[i].country;
-      if (agent_country_code.indexOf(';') !== -1) {
-        var agent_country_code_array = agent_country_code.split('; ');
+      var global_agent_country_code = res[i].country;
+      if (global_agent_country_code.indexOf(';') !== -1) {
+        var global_agent_country_code_array = global_agent_country_code.split('; ');
+        var length = global_agent_country_code_array.length;
       } else {
-        var agent_country_code_array = agent_country_code;
+        var global_agent_country_code_array = global_agent_country_code;
+        var length = 1;
       }
-      for (x = 0; x < agent_country_code_array.length; x++) {
-        var agent_code = agent_country_code_array[x];
-        agent_code = String(agent_code);
+      for (x = 0; x < length; x++) {
+        if (length == 1) {
+          var global_agent_code = global_agent_country_code_array;
+        } else {
+          var global_agent_code = global_agent_country_code_array[x];
+        }
+        global_agent_code = String(global_agent_code);
         var contact_name = res[i].contact_name;
         var email = res[i].email;
         var name = res[i].name;
-        if (country_code == agent_code) {
+        var phone = res[i].phone;
+        var address = res[i].address;
+        if (country_code == global_agent_code) {
           output += '<div class="grid-x grid-padding-x grid-padding-y">';
             output += '<div class="cell medium-12">';
               output += '<div class="shadow card card-section">';
                 output += '<h2 class="h4">';
-                  output += '<small>' + name + '</small><br>' + contact_name;
+                  output += '<span class="h6">' + name + '</span><br>' + contact_name;
                 output += '</h2>';
-                if (email !== '') {
-                  output += '<p><a href="mailto:"' + email + '"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp;' + email + '</a></p>';
+                output += '<p>';
+                  if (email !== '') {
+                    output += '<a href="mailto:"' + email + '"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp;' + email + '</a>';
+                  }
+                  if (phone !== '' && email !== '') {
+                    output += '&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;';
+                  }
+                  if (phone !== '') {
+                    output += '<a href="tel:' + phone + '"><i class="fa fa-phone" aria-hidden="true"></i>&nbsp;&nbsp;Call ' + phone + '</a>';
+                  }
+                output += '</p>';
+                if (address !== '') {
+                  output += '<p class="no-margin-bottom"><strong>Address:</strong></p>';
                 }
+                output += address;
               output += '</div>';
             output += '</div>';
           output += '</div>';
@@ -37,9 +56,9 @@ if (res) {
       }
     }
     if (total > 0) {
-      title += '<hr><h2>Global Agents - %globals_asset_name%</h2><br>';
+      title += '<hr><h2>Global Agents - %globals_asset_name%</h2>';
       print(title);
-      showNavitem = true;
+      print('<style>#global-agent-menu-item{display:block !important;}</style>');
     }
     output += '</section>';
     print(output);
