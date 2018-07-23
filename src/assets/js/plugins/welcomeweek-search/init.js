55462e83b0953f0b6f263f7ebf946117593fd6be
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2018-07-23 19:32:18 +0100

    latest stable build

--- src/assets/js/plugins/welcomeweek-search/init.js
+++ src/assets/js/plugins/welcomeweek-search/init.js
@@ -1,87 +1,5 @@
 jQuery(document).ready(function() {
-
-  /*
-   * Function: Courses A-Z
-   */
-  function coursesAlphabetical() {
-    jQuery('.courses-az').each(function() {
-        jQuery('.page_letters').remove();
-        var lettersHTML = '<div class="page_letters"><ul class="menu" data-magellan data-offset="500">';
-        var lettersArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        var currentLettersArray = [];
-        jQuery(this).children('div').each(function(index, value) {
-            var letter = jQuery(this).data('letter');
-            if (jQuery(this).next('div').length !== 0) {
-                var nextLetter = jQuery(this).next('div').data('letter');
-                if (letter != nextLetter) {
-                    jQuery('<div class="callout primary" data-magellan-target="' + nextLetter + '" id="' + nextLetter + '"><h2 class="h1">' + nextLetter + '</h2></div>').insertAfter(jQuery(this));
-                    currentLettersArray.push(nextLetter);
-                    //lettersHTML += '<a href="#' + nextLetter + '" data-letter="' + nextLetter + '">' + nextLetter + '</a>';
-                    //debug
-                    //console.log('1');
-                }
-            }
-            if (index == 0) {
-                jQuery('<div class="callout primary" data-magellan-target="' + nextLetter + '" id="' + nextLetter + '"><h2 class="h1">' + letter + '</h2></div>').insertBefore(jQuery(this));
-                currentLettersArray.push(letter);
-                //lettersHTML += '<a href="#' + letter + '" data-letter="' + letter + '">' + letter + '</a>';
-                //debug
-                //console.log('1');
-            }
-        });
-        for (var i = 0; i < lettersArray.length; i++) {
-            var letter = lettersArray[i];
-            letter = letter.toUpperCase();
-            //debug
-            var check = jQuery.inArray(letter, currentLettersArray);
-            if (check !== -1) {
-                lettersHTML += '<li data-letter="' + letter + '"><a href=#' + letter + ' data-letter="' + letter + '" class="page-letter-' + letter + '">' + letter + '</a></li>';
-            } else {
-                lettersHTML += '<li data-letter="' + letter + '"><span>' + letter + '</span></li>';
-            }
-        }
-        lettersHTML += '</ul></div>';
-        jQuery('.nav-courses-page-letters').each(function() {
-          jQuery(lettersHTML).insertBefore(jQuery(this));
-        });
-        jQuery('.page_letters a').click(function(event) {
-            jQuery('.page_letters a').removeClass('active');
-            jQuery(this).addClass('active');
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-                location.hostname == this.hostname
-            ) {
-                // Figure out element to scroll to
-                var target = jQuery(this.hash);
-                target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    jQuery('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function() {
-                        // Callback after animation
-                        // Must change focus!
-                        var target = jQuery(target);
-                        target.focus();
-                        if (target.is(":focus")) { // Checking if the target was focused
-                            return false;
-                        } else {
-                            target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                            target.focus(); // Set focus again
-                        };
-                    });
-                }
-            }
-        });
-        /*jQuery('.page_letters div').tsort({
-            attr: 'data-letter'
-        });​*/
-    });
-  }
-
-  function clearLastSearch() {
+  /*function clearLastSearch() {
     var jQuery_GET = {};
     document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function() {
         function decode(s) {
@@ -90,7 +8,7 @@ jQuery(document).ready(function() {
         jQuery_GET[decode(arguments[1])] = decode(arguments[2]);
     });
   }
-  clearLastSearch();
+  clearLastSearch();*/
 
   function getUrlParameter(sParam) {
     var sPageURL = decodeURIComponent(window.location.search.substring(1)),
@@ -117,6 +35,7 @@ jQuery(document).ready(function() {
         //debug for localhost
         //var pageURL = "http://localhost:8000/welcome-week-search.html";
         //update this
+        var selectedLetter = jQuery('.page_letters .active').data('letter'); //left off here
         var searchURL = pageURL + '?query=' + jQuery('#query').val();
         jQuery('#course-finder-new #filters input:checkbox:checked, #course-finder-new #filters input:radio:checked').each(function() {
             // The input names are modified for Funnelback
@@ -173,7 +92,6 @@ jQuery(document).ready(function() {
               }
               ajax = jQuery.get(searchURL, function(data) {
                   updatePageAndHistory(data, this.url, pushState);
-                  coursesAlphabetical();
                   //clearLastSearch();
                   //showMoreCoursesAjax();
               });
@@ -207,6 +125,5 @@ jQuery(document).ready(function() {
   //init
   if (jQuery('#course_list').length) {
     olpAjaxCourseSearch();
-    coursesAlphabetical();
   }
 });
