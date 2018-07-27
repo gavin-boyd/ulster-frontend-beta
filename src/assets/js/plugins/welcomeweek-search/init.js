a7d46c6df9f9e8227a078fd3dd46be489958cd83
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2018-07-27 10:42:59 +0100

    latest stable build

--- src/assets/js/plugins/welcomeweek-search/init.js
+++ src/assets/js/plugins/welcomeweek-search/init.js
@@ -51,7 +51,7 @@ jQuery(document).ready(function() {
         //update this
         var selectedLetter = jQuery('.page_letters .active').data('letter'); //left off here
         //var searchURL = pageURL + '?query=' + jQuery('#query').val() + '&meta_t=' + jQuery('#query').val();
-        var searchURL = pageURL + '?query=t:' + jQuery('#query').val();
+        var searchURL = pageURL + '?query=' + jQuery('#query').val();
         jQuery('#course-finder-new #filters input:checkbox:checked, #course-finder-new #filters input:radio:checked').each(function() {
             // The input names are modified for Funnelback
             var name = jQuery(this).attr('name'),
