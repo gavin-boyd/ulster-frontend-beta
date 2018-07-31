fd39ebd83d95a72655f7e2e2b91e3fa93df8416c
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2018-07-31 12:13:12 +0100

    latest stable build

--- src/assets/js/plugins/welcomeweek-journey/init.js
+++ src/assets/js/plugins/welcomeweek-journey/init.js
@@ -1,3 +1,64 @@
+function welcomeWeekUserActions(user_cookie) {
+  //events logic
+
+  //on load hide all reslife and international events
+  jQuery('.reslife').each(function() {
+    jQuery(this).hide();
+  });
+  jQuery('.international').each(function() {
+    jQuery(this).hide();
+  });
+
+  //cookie varsfor user
+  var u_cookie = user_cookie;
+
+  //user cookie conditionals (events)
+  if (u_cookie) {
+    var u_cookie_array = u_cookie.split(',');
+    var reslife = u_cookie_array[3];
+    var international = u_cookie_array[4];
+
+    //debug
+    console.log('Reslife: ' + reslife);
+    console.log('International: ' + international);
+
+    if (reslife == 1) {
+      //debug
+      console.log('show reslife events');
+
+      jQuery('.reslife').each(function() {
+        jQuery(this).show();
+      });
+    }
+
+    if (reslife == 1) {
+      //debug
+      console.log('hide for reslife');
+
+      jQuery('.reslife').each(function() {
+        jQuery(this).show();
+      });
+
+      jQuery('.reslife-hidden').each(function() {
+        jQuery(this).hide();
+      });
+    }
+
+    if (international == 1) {
+      //debug
+      console.log('show international events');
+
+      jQuery('.international').each(function() {
+        jQuery(this).show();
+      });
+
+      jQuery('.international-hidden').each(function() {
+        jQuery(this).hide();
+      });
+    }
+  }
+}
+
 function getParam(param) {
   var pageURL = window.location.search.substring(1);
   var urlParams = pageURL.split('&');
--- src/assets/js/welcomeweek.js
+++ src/assets/js/welcomeweek.js
@@ -42,5 +42,4 @@ require('./plugins/welcomeweek-search/init');
 require('./plugins/welcomeweek-search/jquery-ui');
 require('./plugins/welcomeweek-search/funnelback-completion');
 require('./plugins/welcomeweek-search/funnelback-completion-init');
-require('./plugins/welcomeweek-journey/actions');
-require('./plugins/welcomeweek-journey/set-cookies');
+require('./plugins/welcomeweek-journey/init');
