3e1062f4c03f9d56d0f79eefaf38142682c131d3
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2018-08-07 18:33:12 +0100

    latest stable build

--- src/assets/js/plugins/click4assistance/init.js
+++ src/assets/js/plugins/click4assistance/init.js
@@ -3,7 +3,7 @@
  */
 jQuery(document).ready(function() {
     function C4AWJSLoaded(guid, wfguid) {
-        var oC4AW_Widget = new oC4AW_Widget();
+        oC4AW_Widget = new oC4AW_Widget();
         oC4AW_Widget.setAccGUID("CD44000A-B9CB-4689-BCDD-0A909F0F65CA");
         oC4AW_Widget.setWSGUID("680c6c21-116b-46ba-b44b-4ef5a76f97a8");
         oC4AW_Widget.setWFGUID(guid);
