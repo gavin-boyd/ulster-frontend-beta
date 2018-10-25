ab09dfb1b6f0a81699f8a3b7a4bc9382033e483e
gavin-boyd <g.boyd@ulster.ac.uk>
2018-10-25 16:50:25 +0100

    latest stable build

--- src/assets/js/plugins/departments-campus-facets/init.js
+++ src/assets/js/plugins/departments-campus-facets/init.js
@@ -5,7 +5,7 @@ function initCampusFiltersOnDepartments() {
     jQuery('#campus-filters a:not(.hollow)').addClass('hollow');
     jQuery(this).removeClass('hollow');
     jQuery('body').prepend('<div class="preloader-container"><div class="preloader"><span class="hide">Loading search results</span> <img src="https://www.ulster.ac.uk/__data/assets/git_bridge/0010/256429/dist/assets/img/ulster-pre-loader.gif" width="169" height="169" alt="Loading results"/></div></div>');
-    jQuery('#staff-cards-results').load(href, function() {
+    jQuery('#staff-cards-results').load(href + ' #staff-cards-results', function() {
       jQuery('.preloader-container').remove();
     });
   });
