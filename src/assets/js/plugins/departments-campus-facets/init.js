9c70f725ac95b7264291ad1cceb80d9a651132c6
gavin-boyd <g.boyd@ulster.ac.uk>
2018-10-25 16:55:44 +0100

    latest stable build

--- src/assets/js/plugins/departments-campus-facets/init.js
+++ src/assets/js/plugins/departments-campus-facets/init.js
@@ -7,6 +7,7 @@ function initCampusFiltersOnDepartments() {
     jQuery('body').prepend('<div class="preloader-container"><div class="preloader"><span class="hide">Loading search results</span> <img src="https://www.ulster.ac.uk/__data/assets/git_bridge/0010/256429/dist/assets/img/ulster-pre-loader.gif" width="169" height="169" alt="Loading results"/></div></div>');
     jQuery('#staff-cards-results').load(href + ' #staff-cards-results', function() {
       jQuery('.preloader-container').remove();
+      jQuery(document).foundation('equalizer', 'reflow');
     });
   });
 }
