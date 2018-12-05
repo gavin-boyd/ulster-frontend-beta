1f836da784d32cf4b6719b908f3f46445e4b5f9c
gavin-boyd <g.boyd@ulster.ac.uk>
2018-12-05 14:12:43 +0000

    latest stable build

--- src/assets/js/plugins/selected-radios/init.js
+++ src/assets/js/plugins/selected-radios/init.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
   jQuery("form.application-form input[type=radio]").each(function() {
     if (jQuery(this).is(":checked")) {
-      jQuery(this).parent('li').css("background", "#006435");
+      jQuery(this).parent('li').css("background", "#005ea5");
       jQuery(this).next('label').css("color", "#fff");
     }
   });
@@ -11,7 +11,7 @@ jQuery(document).ready(function() {
     jQuery('form.application-form input[name="' + name + '"]').parent('li').css("background", "#eee");
     jQuery('form.application-form input[name="' + name + '"]').next('label').css("color", "#0b0c0c");
     if (jQuery(this).is(":checked")) {
-      jQuery(this).parent('li').css("background", "#006435");
+      jQuery(this).parent('li').css("background", "#005ea5");
       jQuery(this).next('label').css("color", "#fff");
     }
   });
