a180a58288952a8579c33384a5920fcc8ece4a41
gavin-boyd <g.boyd@ulster.ac.uk>
2018-12-13 16:57:59 +0000

    latest stable build

---
+++ src/matrix/Ulster.Subjects/Ulster.Subjects (Id: #343836).js
@@ -0,0 +1,10 @@
+var res = eval('(' + _REST.response.body + ')');
+if (res) {
+  function outputSubjects() {
+    var campus = '%nested_get_campus%';
+    var output = '';
+  }
+}
+
+<
+// ditched this to go angular
