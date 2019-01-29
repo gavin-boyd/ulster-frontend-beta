b4e0cc0f907c1750612368204c7f97c1fc567963
gavin-boyd <g.boyd@ulster.ac.uk>
2019-01-29 13:50:01 +0000

    latest stable build

--- dist/assets/js/welcomeweek-journey.js
+++ dist/assets/js/welcomeweek-journey.js
@@ -1,1 +1,319 @@
-!function(e){function r(u){if(o[u])return o[u].exports;var n=o[u]={i:u,l:!1,exports:{}};return e[u].call(n.exports,n,n.exports,r),n.l=!0,n.exports}var o={};return r.m=e,r.c=o,r.i=function(e){return e},r.d=function(e,o,u){r.o(e,o)||Object.defineProperty(e,o,{configurable:!1,enumerable:!0,get:u})},r.n=function(e){var o=e&&e.__esModule?function(){return e["default"]}:function(){return e};return r.d(o,"a",o),o},r.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},r.p="",r(r.s=79)}({61:function(e,r,o){"use strict";o(69)},69:function(e,r,o){"use strict";function u(e){var r=e;if(r){var o=r,u=o[3],n=o[4];1==u&&jQuery(".reslife").each(function(){jQuery(this).show()}),1==u&&(jQuery(".reslife").each(function(){jQuery(this).show()}),jQuery(".reslife-hidden").each(function(){jQuery(this).hide()})),1==n&&(jQuery(".international").each(function(){jQuery(this).show()}),jQuery(".international-hidden").each(function(){jQuery(this).hide()}))}}function n(e){for(var r=window.location.search.substring(1),o=r.split("&"),u=0;u<o.length;u++){var n=o[u].split("=");if(n[0]==e)return n[1]}}function i(e,r,o,u,n){var i=jQuery.cookie("uls_welcome_week_u");i&&jQuery.removeCookie("uls_welcome_week_u",{path:s}),jQuery.cookie.raw=!0,jQuery.cookie("uls_welcome_week_u",[e,r,o,u,n],{expires:c,path:s,domain:a})}function t(e){var r=jQuery.cookie("uls_welcome_week_c");r&&jQuery.removeCookie("uls_welcome_week_c",{path:s}),jQuery.cookie.raw=!0,jQuery.cookie("uls_welcome_week_c",e,{expires:c,path:s,domain:a})}var c=800,a="ulster.ac.uk",s="/",l="https://www.ulster.ac.uk/welcomeweek/_web_services/student-json";jQuery(document).ready(function(){!function(){var e=n("u");if(jQuery(".reslife").each(function(){jQuery(this).hide()}),jQuery(".international").each(function(){jQuery(this).hide()}),jQuery(".reslife").each(function(){jQuery(this).hide()}),jQuery(".international").each(function(){jQuery(this).hide()}),null===e&&""===e||jQuery.getJSON(l,function(r){var o=r.filter(function(r){return r.id==e});o.forEach(function(e,r){var o=e.id,n=e.fname,t=e.sname,c=e.reslife,a=e.international;if(i(o,n,t,c,a),jQuery.cookie("uls_welcome_week_u")){var s=jQuery.cookie("uls_welcome_week_u");s=s.split(","),u(s)}else{var l=[o,n,t,c,a];u(l)}})}),jQuery.cookie("uls_welcome_week_c")?jQuery("#app-toolbar-options").append('<a href="'+jQuery.cookie("uls_welcome_week_c")+'" class="button large expanded rounded no-margin-bottom shadow" id="back-to-course"><i class="fa fa-graduation-cap" aria-hidden="true"></i>&nbsp;&nbsp;View my course</a>'):jQuery("#back-to-course").remove(),jQuery("#course-id").length>0){jQuery("#back-to-course").remove();var r=jQuery("#course-id").data("url");t(r)}jQuery.cookie("uls_welcome_week_u")}()})},79:function(e,r,o){e.exports=o(61)}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 79);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 61:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(69);
+
+/***/ }),
+
+/***/ 69:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var ww_setCookieExpiry = 800;
+//Live
+var ww_setCookieDomain = 'ulster.ac.uk';
+var ww_setCookiePath = '/';
+var ww_api = 'https://www.ulster.ac.uk/welcomeweek/_web_services/student-json';
+//Test
+////const ww_setCookieDomain = 'localhost';
+////const ww_setCookiePath = '';
+////const ww_api = 'http://dct.ulster.ac.uk/welcomeweek/feeds/test.json';
+
+function hideEmptyEventRows() {
+  //debug
+  //console.log('hideEmptyEventRows');
+  jQuery('.ww_events > .grid-x > .cell').each(function () {
+    //debug
+    //console.log('Found a row!');
+    var checkArray = [];
+    jQuery(this).find('.ww_event').each(function () {
+      if (jQuery(this).parent('div').is(':hidden')) {
+        checkArray.push(1);
+      } else {
+        checkArray.push(0);
+      }
+    });
+    if (jQuery.inArray(0, checkArray) == -1) {
+      jQuery(this).hide();
+      //debug
+      //console.log('Found a row with all events are hidden, so hide this row!');
+    }
+    //debug
+    //console.log('Check Array: ' + checkArray);
+  });
+}
+
+function welcomeWeekUserActions(user_cookie) {
+  //events logic
+
+  //debug
+  //jQuery('#debug-user h1 span').text(jQuery.cookie('uls_welcome_week_u'));
+  //jQuery('#debug-course h1 span').text(jQuery.cookie('uls_welcome_week_c'));
+
+  //cookie varsfor user
+  var u_cookie = user_cookie;
+
+  //user cookie conditionals (events)
+  if (u_cookie) {
+    //var u_cookie_array = u_cookie.split(',');
+    var u_cookie_array = u_cookie;
+    var reslife = u_cookie_array[3];
+    var international = u_cookie_array[4];
+
+    //debug
+    //console.log('Reslife: ' + reslife);
+    //console.log('International: ' + international);
+
+    if (reslife == 1) {
+      //debug
+      //console.log('show reslife events');
+
+      jQuery('.reslife').each(function () {
+        jQuery(this).show();
+      });
+    }
+
+    if (reslife == 1) {
+      //debug
+      //console.log('hide for reslife');
+
+      jQuery('.reslife').each(function () {
+        jQuery(this).show();
+      });
+
+      jQuery('.reslife-hidden').each(function () {
+        jQuery(this).hide();
+      });
+    }
+
+    if (international == 1) {
+      //debug
+      //console.log('show international events');
+
+      jQuery('.international').each(function () {
+        jQuery(this).show();
+      });
+
+      jQuery('.international-hidden').each(function () {
+        jQuery(this).hide();
+      });
+    }
+  }
+}
+
+function getParam(param) {
+  var pageURL = window.location.search.substring(1);
+  var urlParams = pageURL.split('&');
+  for (var i = 0; i < urlParams.length; i++) {
+    var paramName = urlParams[i].split('=');
+    if (paramName[0] == param) {
+      return paramName[1];
+    }
+  }
+}
+
+function welcomeWeekPersonCookie(personid, fname, sname, reslife, international) {
+  var cookie = jQuery.cookie('uls_welcome_week_u');
+  if (cookie) {
+    //update cookie
+    jQuery.removeCookie('uls_welcome_week_u', {
+      path: ww_setCookiePath
+    });
+  }
+  jQuery.cookie.raw = true;
+  jQuery.cookie('uls_welcome_week_u', [personid, fname, sname, reslife, international], {
+    expires: ww_setCookieExpiry,
+    path: ww_setCookiePath,
+    domain: ww_setCookieDomain
+  });
+}
+
+function welcomeWeekCourseCookie(url) {
+  var cookie = jQuery.cookie('uls_welcome_week_c');
+  if (cookie) {
+    //update cookie
+    jQuery.removeCookie('uls_welcome_week_c', {
+      path: ww_setCookiePath
+    });
+  }
+  jQuery.cookie.raw = true;
+  jQuery.cookie('uls_welcome_week_c', url, {
+    expires: ww_setCookieExpiry,
+    path: ww_setCookiePath,
+    domain: ww_setCookieDomain
+  });
+}
+
+jQuery(document).ready(function () {
+
+  (function () {
+    /*
+     * global var's
+     */
+    var id = getParam('u');
+
+    //on load
+
+    //on load hide all reslife and international events
+    jQuery('.reslife').each(function () {
+      jQuery(this).hide();
+    });
+    jQuery('.international').each(function () {
+      jQuery(this).hide();
+    });
+
+    //onload hide all reslife and international by default
+    jQuery('.reslife').each(function () {
+      jQuery(this).hide();
+    });
+    jQuery('.international').each(function () {
+      jQuery(this).hide();
+    });
+
+    //set cookies
+    if (id !== null || id !== '') {
+      //set user cookie
+      jQuery.getJSON(ww_api, function (result) {
+        var user = result.filter(function (item) {
+          return item.id == id;
+        });
+        user.forEach(function (key, value) {
+          var id = key.id;
+          var fname = key.fname;
+          var sname = key.sname;
+          var reslife = key.reslife;
+          var international = key.international;
+          welcomeWeekPersonCookie(id, fname, sname, reslife, international);
+          if (jQuery.cookie('uls_welcome_week_u')) {
+            var cookie = jQuery.cookie('uls_welcome_week_u');
+            cookie = cookie.split(',');
+            welcomeWeekUserActions(cookie);
+          } else {
+            var values_array = [id, fname, sname, reslife, international];
+            welcomeWeekUserActions(values_array);
+          }
+          //hideEmptyEventRows();
+        });
+      });
+    }
+
+    //if course cookie is present the display button
+    if (jQuery.cookie('uls_welcome_week_c')) {
+      jQuery('#app-toolbar-options').append('<a href="' + jQuery.cookie('uls_welcome_week_c') + '" class="button large expanded rounded no-margin-bottom shadow" id="back-to-course"><i class="fa fa-graduation-cap" aria-hidden="true"></i>&nbsp;&nbsp;View my course</a>');
+
+      //debug
+      //console.log('show course button');
+      //console.log('show toolbar');
+    } else {
+      jQuery('#back-to-course').remove();
+      //debug
+      //console.log('hide course button');
+    }
+    if (jQuery('#course-id').length > 0) {
+      //remove back to course button on course pages
+      jQuery('#back-to-course').remove();
+      //debug
+      //console.log('hide course button because this is a course page');
+      //set course cookie
+      var courseUrl = jQuery('#course-id').data('url');
+      welcomeWeekCourseCookie(courseUrl);
+    }
+
+    //debug
+    if (jQuery.cookie('uls_welcome_week_u')) {
+      //console.log('user cookie is present');
+      //console.log(jQuery.cookie('uls_welcome_week_u'));
+    } else {}
+      //console.log('user cookie not found');
+
+
+      //hideEmptyEventRows();
+  })();
+});
+
+/***/ }),
+
+/***/ 79:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(61);
+
+
+/***/ })
+
+/******/ });
