b0e4944e8234ded442caf3d667fd1ce3227aff65
gavin-boyd <g.boyd@ulster.ac.uk>
2020-03-11 15:31:03 +0000

    latest stable build

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,19 +1,53 @@
 require('./app');
-//require('./plugins/intl-tel-input/init');
+require('./plugins/intl-tel-input/init');
 require('./plugins/form-dependent-selections/init');
 
 //init tel inputs
-/*jQuery(document).ready(function() {
+jQuery(document).ready(function() {
   jQuery('input[type=tel]').each(function() {
+    if (jQuery(this).data('country')) {
+      var initialCountry = jQuery(this).data('country');
+    } else {
+      var initialCountry = 'gb';
+    }
+    var initialCountry = jQuery(this).data('country');
+    //options
+    // allowDropdown: false,
+    // autoHideDialCode: false,
+    // autoPlaceholder: "off",
+    // dropdownContainer: "body",
+    // excludeCountries: ["us"],
+    // formatOnDisplay: false,
+    // geoIpLookup: function(callback) {
+    //   $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+    //     var countryCode = (resp && resp.country) ? resp.country : "";
+    //     callback(countryCode);
+    //   });
+    // },
+    // hiddenInput: "full_number",
+    // initialCountry: "auto",
+    // localizedCountries: { 'de': 'Deutschland' },
+    // nationalMode: false,
+    // onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+    // placeholderNumberType: "MOBILE",
+    // preferredCountries: ['cn', 'jp'],
+    // separateDialCode: true,
     var options = {
         'utilsScript': '//cdn.ulster.ac.uk/home/ulster-frontend/beta/assets/js/utils.js?v=1.2',
         'formatOnDisplay': true,
         'autoHideDialCode': false,
-        'initialCountry': 'gb',
-        'separateDialCode': true
+        'initialCountry': initialCountry,
+        'separateDialCode': true,
+        'utilsScript': 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/16.0.11/js/utils.js'
     };
     jQuery(this).intlTelInput(options);
-    var number = iti.getNumber();
-    var extension = iti.getExtension();
   });
-});*/
+
+  jQuery("form.application-form").submit(function(e) {
+    //e.preventDefault();
+    jQuery('input[type=tel]').each(function() {
+      var telnumber = jQuery(this).intlTelInput("getNumber", intlTelInputUtils.numberFormat.E164);
+      jQuery(this).val(telnumber);
+    });
+  });
+});
--- src/matrix/Ulster.Coronvirus.Tweet/process.js
+++ src/matrix/Ulster.Coronvirus.Tweet/process.js
@@ -6,7 +6,7 @@ if (json) {
     var text = tweets[i]['text'];
     var url = 'https://twitter.com/' + tweets[i]['user']['screen_name'] + '/status/' + tweets[i]['id_str'];
     var accUrl = 'https://twitter.com/' + tweets[i]['user']['screen_name'];
-    output += '<a href="' + url + '" class="display-block bg-core-blue p-l-15 p-r-15 p-t-10 p-b-10 text-white bl m-b-20" target="_blank">';
+    output += '<a href="' + url + '" class="display-block bg-core-blue p-l-15 p-r-15 p-t-10 p-b-10 text-white bl m-b-20" style="background-color:#1C8DEE!important;" target="_blank">';
     output += '<div class="grid-x">';
     output += '<div class="cell small-2 medium-2 large-2">';
     output += '<span class="fab fa-twitter m-r-10"></span>Latest Tweet';
