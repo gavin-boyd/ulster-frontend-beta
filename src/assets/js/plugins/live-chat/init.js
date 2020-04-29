b9ac6b623d6fe90c778eadcd38078ba5af6c0332
gavin-boyd <g.boyd@ulster.ac.uk>
2020-04-29 07:17:42 +0100

    latest stable build

--- src/assets/js/plugins/live-chat/init.js
+++ src/assets/js/plugins/live-chat/init.js
@@ -16,12 +16,27 @@ jQuery(document).ready(function(){
     var chat_admin = jQuery('#uls-live-chat').data('check');
 
     //object
+    var chat_json_url = decodeURIComponent(jQuery('#uls-chat-data').data('feed'));
     var jsonObj = [];
-    if (jQuery('#q555490_q1').val()) {
-        jsonObj = JSON.parse(jQuery('#q555490_q1').val());
-    } else {
-        jsonObj = [];
-    }
+    jQuery.ajax({
+      url: chat_json_url,
+      dataType: 'json',
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      success: function(data) {
+        if (jQuery('#q555490_q1').val()) {
+            jsonObj = JSON.parse(data);
+        } else {
+            jsonObj = [];
+        }
+      },
+      error: function(xhr, ajaxOptions, thrownError) {
+          jsonObj = [];
+          //debug
+          console.log('error!');
+          console.log(xhr.status);
+          console.log(thrownError);
+      }
+    });
 
     //form
     function doPoll() {
@@ -31,36 +46,35 @@ jQuery(document).ready(function(){
             e.preventDefault(); // prevent native submit
         });
         //update UI on load
-        if (jQuery('#q555490_q1').val()) {
-            var chat_json_url = decodeURIComponent(jQuery('#uls-chat-data').data('feed'));
-            jQuery.ajax({
-                url: chat_json_url,
-                dataType: 'json',
-                contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-                success: function(data) {
-                    var chat_content = '';
-                    //debug
-                    //console.log('success!');
-                    //console.log(data);
-                    //loop
-                    jQuery.each(data, function(key, value) {
-                        if (value.admin == false) {
-                            chat_content += '<div class="cell large-8"><span class="fas fa-user" aria-hidden="true"></span>&nbsp;&nbsp;' + value.message + '</div>';
-                        }
-                        if (value.admin == true) {
-                            chat_content += '<div class="cell large-8 large-offset-4 text-right">' + value.message + '&nbsp;&nbsp;<img src="https://cdn.ulster.ac.uk/home/ulster-frontend/favicons/apple-touch-icon-57x57.png" width="30" /></div>';
-                        }
-                    });
-                    chat_container.html(chat_content);
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    //debug
-                    console.log('error!');
-                    console.log(xhr.status);
-                    console.log(thrownError);
-                }
-            });
-         }
+        var chat_json_url = decodeURIComponent(jQuery('#uls-chat-data').data('feed'));
+        jQuery.ajax({
+            url: chat_json_url,
+            dataType: 'json',
+            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            success: function(data) {
+                var chat_content = '';
+                //debug
+                //console.log('success!');
+                //console.log(data);
+                //loop
+                jQuery.each(data, function(key, value) {
+                    if (value.admin == false) {
+                        chat_content += '<div class="cell large-8"><span class="fas fa-user" aria-hidden="true"></span>&nbsp;&nbsp;' + value.message + '</div>';
+                    }
+                    if (value.admin == true) {
+                        chat_content += '<div class="cell large-8 large-offset-4 text-right">' + value.message + '&nbsp;&nbsp;<img src="https://cdn.ulster.ac.uk/home/ulster-frontend/favicons/apple-touch-icon-57x57.png" width="30" /></div>';
+                    }
+                });
+                chat_container.html(chat_content);
+                jQuery('#q555490_q1').val(data);
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                //debug
+                console.log('error!');
+                console.log(xhr.status);
+                console.log(thrownError);
+            }
+        });
       }
       setTimeout(doPoll,5000);
     }
@@ -68,37 +82,62 @@ jQuery(document).ready(function(){
 
     jQuery('#uls-send-chat-msg').on('click', function(e) {
         e.preventDefault();
-        //DEBUG
-        console.log('send message !');
         var msg = jQuery('input[name=uls-chat-text]').val();
         item = {}
         item ['userid'] = userID;
-        item ['message']  = msg;
-        item ['admin']  = chat_admin;
-        jsonObj.push(item);
-        var jsonString = JSON.stringify(jsonObj);
-        jQuery('#q555490_q1').val(jsonString);
+        item ['message'] = msg;
+        item ['admin'] = chat_admin;
+        var chat_json_url = decodeURIComponent(jQuery('#uls-chat-data').data('feed'));
+        jQuery.ajax({
+          url: chat_json_url,
+          dataType: 'json',
+          contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+          success: function(data) {
+            data.push(item);
+            jQuery('#q555490_q1').val(JSON.stringify(data));
+          },
+          error: function(xhr, ajaxOptions, thrownError) {
+            //debug
+            console.log('error!');
+            console.log(xhr.status);
+            console.log(thrownError);
+          }
+        });
         //prep form
         jQuery('#form_email_555463').ajaxForm(function() {
+           //debug
            console.log("Form prepped!");
         });
         //submit form
         jQuery('#form_email_555463').ajaxSubmit(function() {
-            console.log("Form submitted!");
-            //update UI
-            var chat_json = jsonObj;
-            chat_container.html('');
-            jQuery.each(chat_json, function(key, value) {
-                if (value.admin == false) {
-                    //debug
-                    console.log('user said:' + value.message);
-                    chat_container.append('<div class="cell large-8"><span class="fas fa-user" aria-hidden="true"></span>&nbsp;&nbsp;' + value.message + '</div>');
-                }
-                if (value.admin == true) {
-                    //debug
-                    console.log('admin said:' + value.message);
-                    chat_container.append('<div class="cell large-8 large-offset-4 text-right">' + value.message + '&nbsp;&nbsp;<img src="https://cdn.ulster.ac.uk/home/ulster-frontend/favicons/apple-touch-icon-57x57.png" width="30" /></div>');
-                }
+            var chat_json_url = decodeURIComponent(jQuery('#uls-chat-data').data('feed'));
+            jQuery.ajax({
+              url: chat_json_url,
+              dataType: 'json',
+              contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+              success: function(data) {
+                  var chat_content = '';
+                  //debug
+                  //console.log('success!');
+                  console.log(data);
+                  //loop
+                  jQuery.each(data, function(key, value) {
+                      if (value.admin == false) {
+                          chat_content += '<div class="cell large-8"><span class="fas fa-user" aria-hidden="true"></span>&nbsp;&nbsp;' + value.message + '</div>';
+                      }
+                      if (value.admin == true) {
+                          chat_content += '<div class="cell large-8 large-offset-4 text-right">' + value.message + '&nbsp;&nbsp;<img src="https://cdn.ulster.ac.uk/home/ulster-frontend/favicons/apple-touch-icon-57x57.png" width="30" /></div>';
+                      }
+                  });
+                  chat_container.html(chat_content);
+                  jQuery('#q555490_q1').val(JSON.stringify(data));
+              },
+              error: function(xhr, ajaxOptions, thrownError) {
+                  //debug
+                  console.log('error!');
+                  console.log(xhr.status);
+                  console.log(thrownError);
+              }
             });
             //clear chat text
             jQuery('input[name=uls-chat-text]').val('');
